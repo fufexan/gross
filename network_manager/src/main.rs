61b30c552bcdaee26f09ccabf48f55542841d362
Mihai Fufezan <fufexan@protonmail.com>
2023-11-16 00:38:54 +0200

    network_manager: try using stream

--- network_manager/src/main.rs
+++ network_manager/src/main.rs
@@ -1,3 +1,4 @@
+use futures_util::stream::StreamExt;
 use std::error::Error;
 use zbus::{dbus_proxy, zvariant::ObjectPath, Connection, Result};
 
@@ -11,17 +12,17 @@ trait NetworkManager {
     fn primary_connection(&self) -> Result<ObjectPath>;
 }
 
-#[dbus_proxy(
-    interface = "org.freedesktop.NetworkManager.Connection.Active",
-    default_service = "org.freedesktop.NetworkManager",
-    default_path = "/org/freedesktop/NetworkManager/ActiveConnection/0"
-)]
-trait ActiveConnection {
-    #[dbus_proxy(property)]
-    fn id(&self) -> Result<String>;
-    #[dbus_proxy(property, name = "Type")]
-    fn connection_type(&self) -> Result<String>;
-}
+// #[dbus_proxy(
+//     interface = "org.freedesktop.NetworkManager.Connection.Active",
+//     default_service = "org.freedesktop.NetworkManager",
+//     default_path = "/org/freedesktop/NetworkManager/ActiveConnection/0"
+// )]
+// trait ActiveConnection {
+//     #[dbus_proxy(property)]
+//     fn id(&self) -> Result<String>;
+//     #[dbus_proxy(property, name = "Type")]
+//     fn connection_type(&self) -> Result<String>;
+// }
 
 #[tokio::main]
 async fn main() -> std::result::Result<(), Box<dyn Error>> {
@@ -29,25 +30,31 @@ async fn main() -> std::result::Result<(), Box<dyn Error>> {
 
     let nm_proxy = NetworkManagerProxy::new(&connection).await?;
 
-    let primary_connection = nm_proxy.primary_connection().await?;
-
-    log::debug!("New primary connection: {}", primary_connection);
-
-    let primary_connection_proxy = ActiveConnectionProxy::builder(&connection)
-        .path(primary_connection)?
-        .build()
-        .await?;
-
-    let id = primary_connection_proxy.id().await?;
-    log::debug!("id: {:?}", id);
-
-    let connection_type = primary_connection_proxy.connection_type().await?;
-    log::debug!("type: {connection_type}");
-
-    // event loop
     let mut nm_stream = nm_proxy.receive_primary_connection_changed().await;
     while let Some(e) = nm_stream.next().await {
-        println!("{:?}", e.get().await);
+        let ev = e.get().await;
+        println!("{:?}", ev);
+
+        // let primary_connection = nm_proxy.primary_connection().await?;
+
+        // log::debug!("New primary connection: {}", primary_connection);
+
+        // let primary_connection_proxy = ActiveConnectionProxy::builder(&connection)
+        //     .path(primary_connection)?
+        //     .build()
+        //     .await?;
+
+        // let id = primary_connection_proxy.id().await?;
+        // log::debug!("id: {:?}", id);
+
+        // let connection_type = primary_connection_proxy.connection_type().await?;
+        // log::debug!("type: {connection_type}");
+
+        // // event loop
+        // let mut connection_stream = primary_connection_proxy.receive_id_changed().await;
+        // while let Some(e) = connection_stream.next().await {
+        //     println!("{:?}", e.get().await);
+        // }
     }
 
     Ok(())
